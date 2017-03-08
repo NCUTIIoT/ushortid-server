e0f82c0fc6ab68dbf66d9fefba62eb27e340d0fb
neol-d2022 <neol_d2022@outlook.com>
2017-03-08 21:15:25 +0800

    Parse DAGroots' addr from param.

--- main.c
+++ main.c
@@ -32,14 +32,33 @@ static void wair_thread(pthread_t *outPid)
 }
 
 unsigned int nRoots;
-char **roots;
+unsigned char **roots;
 int main(int argc, char **argv)
 {
+    unsigned int buf[8];
     pthread_t ushortid_svr, uhurricane_listener;
-    unsigned int ushortid_svr_port = 15004, uhurricane_listener_port = 15003;
+    unsigned int ushortid_svr_port = 15004, uhurricane_listener_port = 15003, i, j;
 
     nRoots = argc - 1;
-    roots = argv + 1;
+    roots = (unsigned char **)malloc(sizeof(*roots) * nRoots);
+    for (i = 0; i < nRoots; i += 1)
+    {
+        roots[i] = (unsigned char *)malloc(8);
+        if (sscanf(argv[i + 1], "%x:%x:%x:%x:%x:%x:%x:%x", buf + 0, buf + 1, buf + 2, buf + 3, buf + 4, buf + 5, buf + 6, buf + 7) != 8)
+        {
+            fprintf(stderr, "[ERROR] '%s' is not a 8-byte address.\n", argv[i + 1]);
+            return 1;
+        }
+        for (j = 0; j < 8; j += 1)
+        {
+            if (buf[j] > 0xFF)
+            {
+                fprintf(stderr, "[ERROR] '%s' is not a 8-byte address.\n", argv[i + 1]);
+                return 1;
+            }
+            *(roots[i] + j) = (unsigned char)buf[j];
+        }
+    }
 
     launch_thread(&ushortid_svr, ushortid_server_entry, &ushortid_svr_port);
     launch_thread(&uhurricane_listener, uhurricane_listener_entry, &uhurricane_listener_port);
