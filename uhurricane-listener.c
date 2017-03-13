2ff3d0a5283e490cd5d112060dc453a43e8cd482
neol-d2022 <neol_d2022@outlook.com>
2017-03-13 20:16:06 +0800

    Encourage mote to explore more about neighbors.

--- uhurricane-listener.c
+++ uhurricane-listener.c
@@ -126,8 +126,7 @@ static int uhurricane_listener(unsigned int port)
                     if (*((unsigned char *)buf + (i + 4)) > 0)
                         pdr = (double)(*((unsigned char *)buf + (i + 5))) / (double)(*((unsigned char *)buf + (i + 4)));
                     else
-                        //pdr = 0.0;
-                        pdr = 1.0 / 256.0;
+                        pdr = 1.0;
                     nei = (MOTE_REPORT_NEI *)malloc(sizeof(*nei));
                     nei->pdr = pdr;
                     if (cmp(c, r) > 0)
